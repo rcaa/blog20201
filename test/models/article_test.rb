3ee16919e44e73dc07998cba085c8126d7737169
Rodrigo Andrade <rodrigocaa@gmail.com>
2020-01-29 16:27:43 -0300

    test and gemfile

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save article without title' do
+    article = Article.new
+    assert_not article.save
+  end
+
+  test 'should save article with correct title' do
+    article = Article.new title: 'Ruby is awesome'
+    assert article.save
+  end
 end
